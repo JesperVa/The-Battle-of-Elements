d7d8cc15c576334867276757c49a3dba98641b4d
khovstadius <khovstadius@hotmail.com>
2017-11-16 17:56:00 +0100

    Fixade tillbaka färgen på pl4 uin

--- Assets/Scripts/UI/UIKnockbackScript.cs
+++ Assets/Scripts/UI/UIKnockbackScript.cs
@@ -18,6 +18,8 @@ public class UIKnockbackScript : MonoBehaviour {
 	void Update () 
 	{
 		m_playerKnockbackText.text = GameMasterScript.Instance.GetPlayerKnockback (m_player).ToString();
+        // Om den andra lösningen blev för farlig kan vi pröva detta istället
+        //m_playerKnockbackText.text = m_player.Knockback.ToString();
 	}
 }
 
