8aae2a367e8cc806444c611807ad81cf304ef182
JesperVa <Smoovefan@hotmail.se>
2017-11-16 17:19:43 +0100

    Changes to singleton script

--- Assets/Scripts/SingletonScripts/SingletonScript.cs
+++ Assets/Scripts/SingletonScripts/SingletonScript.cs
@@ -10,76 +10,73 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class SingletonScript<T> : MonoBehaviour where T : MonoBehaviour
 {
-	private static T instance;
+    private static T instance;
 
-	private static object Lock = new object();
+    private static object Lock = new object();
 
-	private static bool ApplicationIsQuitting = false;
+    private static bool ApplicationIsQuitting = false;
 
-	public static T Instance
-	{
-		get
-		{
-			if (ApplicationIsQuitting) 
-			{
-				Debug.LogWarning("[SingletonScript] Instance '"+ typeof(T) +
-					"' already destroyed on application quit." +
-					" Won't create again - returning null.");
-				return null;
-			}
-
-			lock(Lock)
-			{
-                
-				if (instance == null)
-				{
-					instance = (T) FindObjectOfType(typeof(T));
-
-					if ( FindObjectsOfType(typeof(T)).Length > 1 )
-					{
-						Debug.LogError("[Singleton] Something went really wrong " +
-							" - there should never be more than 1 singleton!" +
-							" Reopening the scene might fix it.");
-						return instance;
-					}
-
-					if (instance == null)
-					{
-						GameObject singleton = new GameObject();
-						instance = singleton.AddComponent<T>();
-						singleton.name = "(singleton) "+ typeof(T).ToString();
-
-						DontDestroyOnLoad(singleton);
-
-						Debug.Log("[Singleton] An instance of " + typeof(T) + 
-							" is needed in the scene, so '" + singleton +
-							"' was created with DontDestroyOnLoad.");
-					} 
-					else 
-					{
-						Debug.Log("[Singleton] Using instance already created: " +
-							instance.gameObject.name);
-					}
-				}
-
-				return instance;
-			}
-		}
-	}
-
-	/// <summary>
-	/// When Unity quits, it destroys objects in a random order.
-	/// In principle, a Singleton is only destroyed when application quits.
-	/// If any script calls Instance after it have been destroyed, 
-	///   it will create a buggy ghost object that will stay on the Editor scene
-	///   even after stopping playing the Application. Really bad!
-	/// So, this was made to be sure we're not creating that buggy ghost object.
-	/// </summary>
-	public void OnDestroy () 
-	{
-        if (SceneManager.GetActiveScene().name != "MainMenu")
+    public static T Instance
+    {
+        get
         {
-            ApplicationIsQuitting = true;
+            //if (ApplicationIsQuitting)
+            //{
+            //    Debug.LogWarning("[SingletonScript] Instance '" + typeof(T) +
+            //        "' already destroyed on application quit." +
+            //        " Won't create again - returning null.");
+            //    return null;
+            //}
+
+            lock (Lock)
+            {
+
+                if (instance == null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("[Singleton] Something went really wrong " +
+                            " - there should never be more than 1 singleton!" +
+                            " Reopening the scene might fix it.");
+                        return instance;
+                    }
+
+                    if (instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+
+                        DontDestroyOnLoad(singleton);
+
+                        Debug.Log("[Singleton] An instance of " + typeof(T) +
+                            " is needed in the scene, so '" + singleton +
+                            "' was created with DontDestroyOnLoad.");
+                    }
+                    else
+                    {
+                        Debug.Log("[Singleton] Using instance already created: " +
+                            instance.gameObject.name);
+                    }
+                }
+
+                return instance;
+            }
         }
-	}
+    }
+
+    /// <summary>
+    /// When Unity quits, it destroys objects in a random order.
+    /// In principle, a Singleton is only destroyed when application quits.
+    /// If any script calls Instance after it have been destroyed, 
+    ///   it will create a buggy ghost object that will stay on the Editor scene
+    ///   even after stopping playing the Application. Really bad!
+    /// So, this was made to be sure we're not creating that buggy ghost object.
+    /// </summary>
+    public void OnDestroy()
+    {
+        ApplicationIsQuitting = true;
+    }
 }
