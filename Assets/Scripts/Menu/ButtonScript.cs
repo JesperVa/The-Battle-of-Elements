9ef03160fed423165f544ee52199fdbc1b03219b
khovstadius <khovstadius@hotmail.com>
2018-01-04 13:57:59 +0100

    color of buttons in menu

---
+++ Assets/Scripts/Menu/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public Toggle m_toggle;
+    public Image m_backgoundImage;
+	// Use this for initialization
+	void Start ()
+    {
+        
+        //m_backgoundImage.color = new Color32(0, 0, 255, 200);
+
+    }
+	
+    public void ChangeBackground()
+        {
+            m_backgoundImage.color = new Color32(0, 255, 0, 200);
+        }
+    
+            
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_toggle.isOn)
+            ChangeBackground();
+        else
+            m_backgoundImage.color = new Color32(255, 0, 0, 200);
+    }
+}
